f317ce9d92beb2d565c26b62a5cc848282004c25
Glazkova_Marina <glazkova.mv@bk.ru>
2022-06-07 22:43:46 +0300

    add task 8

--- Program.cs
+++ Program.cs
@@ -50,26 +50,51 @@
 // Console.WriteLine("max = " + max);
 
 
-// Задача 6: Напишите программу, которая на вход
-// принимает число и выдаёт, является ли число чётным
-// (делится ли оно на два без остатка).
-// 4 -> да
-// -3 -> нет
-// 7 -> нет
+// // Задача 6: Напишите программу, которая на вход
+// // принимает число и выдаёт, является ли число чётным
+// // (делится ли оно на два без остатка).
+// // 4 -> да
+// // -3 -> нет
+// // 7 -> нет
 
-Console.Write("Input integer number a: ");
-int a = int.Parse(Console.ReadLine());
-if (a % 2 == 0)
-{
-    Console.WriteLine("yes");
-}
-else
-{
-    Console.WriteLine("no");
-}
+// Console.Write("Input integer number a: ");
+// int a = int.Parse(Console.ReadLine());
+// if (a % 2 == 0)
+// {
+//     Console.WriteLine("yes");
+// }
+// else
+// {
+//     Console.WriteLine("no");
+// }
 
 // Задача 8: Напишите программу, которая на вход
 // принимает число (N), а на выходе показывает все чётные
 // числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+int N = -1;
+while (N <= 0) // check our user
+{
+    Console.Write("Input positive integer number N: ");
+    N = int.Parse(Console.ReadLine());
+    if (N <= 0)
+    {
+        Console.WriteLine("Incorrect input!");
+    }
+}
+
+if (N != 1)
+{
+    int i = 2;
+    while(i <= N)
+    {
+        Console.Write(i + " ");
+        i += 2;
+    }
+}
+else
+{
+    Console.WriteLine("There are not even numbers.");
+}
+
