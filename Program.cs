9ae598b0347a816aa72d4d1fab213fa3df7b750a
Glazkova_Marina <glazkova.mv@bk.ru>
2022-06-07 22:28:47 +0300

    add task 4

--- Program.cs
+++ Program.cs
@@ -28,6 +28,28 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Input integer number a: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Input integer number b: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Input integer number c: ");
+int c = int.Parse(Console.ReadLine());
+int max = -9999999;
+if (a >= b & a >= c)
+{
+    max = a;
+}
+else if (b > a & b >= c)
+{
+    max = b;
+}
+else
+{
+    max = c;
+}
+Console.WriteLine("max = " + max);
+
+
 // Задача 6: Напишите программу, которая на вход
 // принимает число и выдаёт, является ли число чётным
 // (делится ли оно на два без остатка).
